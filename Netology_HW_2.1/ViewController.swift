9190f06312afd76de7d22aac8a98dde5444aace9
Dima Gorbachev <dimagorbachev@192.168.1.5>
2021-12-06 22:01:16 +0300

    init commit

---
+++ Netology_HW_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_HW_2.1
+//
+//  Created by Dima Gorbachev on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
